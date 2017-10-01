6cb0224ed693b0869d95a0cdb8776922e76a9d01
Samuel Paz Mendes <pazspm@gmail.com>
2017-10-01 14:17:08 -0300

    Update comments on ARFFCreator.java

--- src/ARFFCreator.java
+++ src/ARFFCreator.java
@@ -7,23 +7,9 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Example class that converts HTML files stored in a directory structure into 
- * and ARFF file using the TextDirectoryLoader converter. It then applies the
- * StringToWordVector to the data and feeds a J48 classifier with it.
- *
- * @author FracPete (fracpete at waikato dot ac dot nz)
- */
+
 public class ARFFCreator {
 
-	/**
-	 * Expects the first parameter to point to the directory with the text files.
-	 * In that directory, each sub-directory represents a class and the text
-	 * files in these sub-directories will be labeled as such.
-	 *
-	 * @param args        the commandline arguments
-	 * @throws Exception  if something goes wrong
-	 */
 	public static void main(String[] args) throws Exception {
 
 		int i = 1;
